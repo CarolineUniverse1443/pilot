9a013afc3e56adc54dc01b0494ed7b5da961102e
CarolineUniverse1443 <popovakaro03@gmail.com>
2023-02-03 17:38:24 +0300

    Add test page

---
+++ test.php
@@ -0,0 +1,167 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
+	<section class="checkouts">
+		<div class="container">
+			<div class="checkouts__wrap">
+				<h2>Какие бывают кассы самообслуживания</h2>
+				<ul class="checkouts__list">
+					<li class="checkout__list-item">
+						<img class="checkout__list-item-img" src="images/floor_checkout.png" alt="floor_checkout">
+						<h3>C напольным исполнением</h3>
+						<p>Стандартная модель кассы самообслуживания для магазинов средней и большой площади, выпускается в настенном и напольном исполнениях.</p>
+					</li>
+					<li class="checkout__list-item">
+						<img class="checkout__list-item-img" src="images/wall_checkout.png" alt="wall_checkout">
+						<h3>С настенным исполнением</h3>
+						<p>Стандартная модель кассы самообслуживания для магазинов средней и большой площади, выпускается в настенном и напольном исполнениях.</p>
+					</li>
+					<li class="checkout__list-item">
+						<img class="checkout__list-item-img" src="images/table_checkout.png" alt="table_checkout">
+						<h3>С настольным исполнение</h3>
+						<p>Стандартная модель кассы самообслуживания для магазинов средней и большой площади, выпускается в настенном и напольном исполнениях.</p>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</section>
+
+	<section class="select">
+		<div class="container">
+			<div class="select__wrap">
+				<div class="select__desc">
+					<h2 class="select__heading">Как выбрать кассу самообслуживания</h2>
+					<p>Перед покупкой и установкой кассы самообслуживания вам нужно подобрать подходящие компоненты. Благодаря нашему конфигуратору вы можете собрать свою КСО из предложенных компонентов</p>
+					<img src="images/choice.png" alt="choice">
+				</div>
+				<div class="select__manual">
+					<ul class="select__manual-list">
+						<li> <p class="select__manual-stage select__manual-stage-01"> <a href="https://www.pilot.ru/catalog/kassa-samoobsluzhivaniya/configurator/">откройте конфигуратор КСО</a> </p> </li>
+						<li> <p class="select__manual-stage select__manual-stage-02"> выберите модель и крепление </p> </li>
+						<li> <p class="select__manual-stage select__manual-stage-03"> выберите процессор, ОЗУ и др. компоненты </p> </li>
+						<li> <p class="select__manual-stage select__manual-stage-04"> отправьте получившуюся конфигурацию нашим специалистам </p> </li>
+						<li> <p class="select__manual-stage select__manual-stage-05"> получите коммерческое предложение </p> </li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="padding giftparallax blueparallax" id="giftparallax">
+		<div class="parallax_wrapper">
+			<div class="giftbganim" data-mod="0.8" style="background-image: url(&quot;/local/templates/pilot/images/CTA-2.png&quot;); background-position: 58.92px 16.6515px;">
+			</div>
+			<div class="giftbganim" data-mod="0.5" style="background-image: url(&quot;/local/templates/pilot/images/CTA-3.png&quot;); background-position: 36.825px 10.4072px;">
+			</div>
+			<div class="giftbganim" data-mod="0.3" style="background-image: url(&quot;/local/templates/pilot/images/CTA-4.png&quot;); background-position: 22.095px 6.24432px;">
+			</div>
+			<div class="giftbganim" data-mod="0.1" style="background-image: url(&quot;/local/templates/pilot/images/CTA-5.png&quot;); background-position: 7.365px 2.08144px;">
+			</div>
+		</div>
+		<div class="container">
+			<div class="row equal">
+				<div class="col-sm-6 col-sm-8 col-xs-12">
+					<p class="h1 white">
+						 Посмотрите вживую!
+					</p>
+					<p>
+						Прежде чем решиться на покупку оборудования от «Пилота», <br> убедитесь в его качестве: закажите презентацию интересующих устройств и сопутствующего ПО у&nbsp;нас или у&nbsp;вас в&nbsp;офисе.
+					</p>
+		 			<a href="#custom-form-popup" class="btn white giftparallax__btn">Заказать демонстрацию</a>
+					<p id="set-modal-name" hidden="">
+						 Запрос презентации
+					</p>
+				</div>
+				<div class="col-sm-6 col-sm-4 col-xs-12 giftbg giftparallax__giftbg" >
+				</div>
+			</div>
+		</div>
+	 </section>
+
+	 <section class="advantage">
+	 	<div class="container">
+	 		<div class="advantage__wrap">
+	 			<h2>Преимущества касс самообслуживания</h2>
+	 			<div class="advantage__list">
+	 				<div class="advantage__list-item">
+	 					<img src="images/advantage_1.png" alt="advantage_1">
+	 					<h3>Сокращение очередей до 40%</h3>
+	 					<p>54% покупателей считают, что тратят впустую от 30 минут до 4 часов в неделю, простаивая в очередях. Около 64% покупателей покидают магазин из-за очередей в кассу.  Self-Checkout позволяет сократить очереди на 40%</p>
+	 				</div>
+	 				<div class="advantage__list-item">
+	 					<img src="images/advantage_2.png" alt="advantage_2">
+	 					<h3>Эффективное распределение потока</h3>
+	 					<p class="advantage__list-item-short">Двух кассиров, работающих на классическом кассовом узле, заменяет 1 консультант-помощник.</p>
+	 				</div>
+	 				<div class="advantage__list-item">
+	 					<img src="images/advantage_3.png" alt="advantage_3">
+	 					<h3>Оптимизация затрат</h3>
+	 					<p class="advantage__list-item-short">Два классических кассовых узла можно заменить на 4 Self-Checkout. Станция с безналичной оплатой самая компактная и ее цена в 2,5 раза дешевле, чем Self-Checkout с функцией приема наличных. </p>
+	 				</div>
+	 				<div class="advantage__list-item">
+	 					<img src="images/advantage_4.png" alt="advantage_4">
+	 					<h3>Рекламные возможности</h3>
+	 					<p>Размещение рекламных роликов на дисплеях терминалов позволяет привлечь внимание нужной целевой аудитории и сократить расходы на проведение рекламных кампаний.</p>
+	 				</div>
+	 				<div class="advantage__list-item">
+	 					<img src="images/advantage_5.png" alt="advantage_5">
+	 					<h3>Снижение нагрузки на персонал</h3>
+	 					<p class="advantage__list-item-short">Двух кассиров, работающих на классическом кассовом узле, заменяет 1 консультант-помощник.</p>
+	 				</div>
+	 			</div>
+	 		</div>
+	 	</div>
+	 </section>
+
+	 <section class="cost">
+	 	<div class="container">
+	 		<div class="cost__wrap">
+	 			<h2>Сколько стоит касса самообслуживания</h2>
+	 			<p>ГК “Пилот” предлагает разнообразные варианты монтажа: на столешницу, на напольную тумбу или на стену, а также установку с внесением наличных или с безналичной оплатой.</p>
+	 			<div class="cost__images">
+	 				<div class="cost__images-wrap"> <img src="images/cost_1.png" alt="cost_1"> </div>
+	 				<div class="cost__images-wrap"> <img src="images/cost_2.png" alt="cost_2"> </div>
+	 				<div class="cost__images-wrap"> <img src="images/cost_3.png" alt="cost_3"> </div>
+	 			</div>
+ 				<div class="cost__config"> <a href="#" class="cost__btn">Сконфигурировать <span>свою кассу</span></a> </div>
+	 		</div>
+	 	</div>
+	 </section>
+
+	 <section class="profit">
+	 	<div class="container">
+	 		<div class="profit__wrap">
+	 			<div class="profit__program">
+	 				<h2>Кассовая программа “Профи-Т Смарт” для касс самообслуживания</h2>
+	 				<p>Группа компаний “Пилот” внедряет современные и качественные системы Self-Checkout под любые потребности и  пожелания заказчика от серийных моделей до индивидуальных проетов.</p>
+	 				<p>В сочетании с профессиональным ПО “Профи Т Смарт” вы получите идеальный инструмент для обеспечения самообслуживания в вашем магазине.</p>
+	 				<a href="#" class="profit__btn">Запросить демо</a>
+	 				<a href="#" class="profit__btn profit__btn-license">Купить лицензию</a>
+	 			</div>
+	 			<div class="profit__image">
+	 				<img src="images/profit.png" alt="profit">
+	 			</div>
+	 		</div>
+	 	</div>
+	 </section>
+
+	 <section class="findout">
+	 	<div class="container">
+	 		<div class="findout__wrap">
+	 			<h2>Узнайте больше о кассах самообслуживания</h2>
+	 			<div class="findout__blocks">
+	 				<div class="findout__block">
+		 				<img src="images/manual.png" alt="manual">
+		 				<a href="#">Кассы самообслуживания в 2021 году: подробное руководство</a>
+	 				</div>
+	 				<div class="findout__block">
+		 				<img src="images/stealing.png" alt="stealing">
+		 				<a href="#">Как системы самообслуживания решают проблемы воровства в магазинах</a>
+	 				</div>
+	 				<div class="findout__block">
+		 				<img src="images/stealing.png" alt="stealing">
+		 				<a href="#">Как системы самообслуживания решают проблемы воровства в магазинах</a>
+		 			</div>
+	 			</div>
+	 		</div>
+	 	</div>
+	 </section>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
